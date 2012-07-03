cf3ceba7886cd0836cc9b40c4215cb91e2e1d2ca
George Redinger <george@georgeredinger.com>
2012-07-02 21:15:26 -0700

    ignore .bundle and .git

--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-s3cmd -P -r --exclude "*.svg" --delete-removed  sync . s3://yogasandpoint.com/
+s3cmd -P -r --exclude "*.svg" --exclude ".bundle/*"  --exclude ".git/*" --delete-removed  sync . s3://yogasandpoint.com/
 
